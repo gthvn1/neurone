ffe17d79eb5bd95afa35bf1b29c2bcf105875984
Guillaume <guillaume.thouvenin@vates.tech>
2026-02-15 21:44:25 +0100

    maj

--- src/neurone.jl
+++ src/neurone.jl
@@ -3,11 +3,11 @@ function sigmoid(z)
 end
 
 function loss(y, t)
-    1/2 * (y - t)^2
+    1 / 2 * (y - t)^2
 end
 
 function gradient(w1, w2, b, x1, x2, t)
-    z = w1*x1 + w2 * x2 + b
+    z = w1 * x1 + w2 * x2 + b
     y = sigmoid(z)
 
     dE_dy = y - t
@@ -22,3 +22,24 @@ function gradient(w1, w2, b, x1, x2, t)
 
     dE_dw1, dE_dw2, dE_db
 end
+
+function learning_step(w1, w2, b, x1, x2, t, learning_rate)
+    dE_dw1, dE_dw2, dE_db = gradient(w1, w2, b, x1, x2, t)
+    w1 -= learning_rate * dE_dw1
+    w2 -= learning_rate * dE_dw2
+    b -= learning_rate * dE_db
+    w1, w2, b
+end
+
+# Initial values
+w1 = 0.5
+w2 = 0.5
+b = -0.5
+
+# Expected results
+data = [
+    (0, 0, 0),
+    (0, 1, 0),
+    (1, 0, 0),
+    (1, 1, 1)
+]

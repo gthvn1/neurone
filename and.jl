0508482d5b585db0fca703fb28e8d75e5af11d7f
Guillaume <guillaume.thouvenin@vates.tech>
2026-02-16 15:43:00 +0100

    utilisation d'une structure neurone

--- and.jl
+++ and.jl
@@ -1,5 +1,10 @@
 using LinearAlgebra
 
+struct Neuron
+    w::Vector{Float64}
+    b::Float64
+end
+
 function sigmoid(z::Float64)
     return 1 / (1 + exp(-z))
 end
@@ -10,8 +15,8 @@ end
 
 # z = w1 * x1 + w2 * x2 + b
 #   On fait le produit scalaire de w et x et on ajoute b
-function predict(w::Vector{Float64}, b::Float64, x::Vector{Float64})
-    sigmoid(dot(w, x) + b)
+function predict(neuron::Neuron, x::Vector{Float64})
+    sigmoid(dot(neuron.w, x) + neuron.b)
 end
 
 function gradient(w::Vector{Float64}, b::Float64, data::Tuple{Vector{Float64},Float64})
@@ -27,73 +32,68 @@ function gradient(w::Vector{Float64}, b::Float64, data::Tuple{Vector{Float64},Fl
 end
 
 function one_step(
-    w::Vector{Float64},
-    b::Float64,
+    neuron::Neuron,
     data::Tuple{Vector{Float64},Float64},
     learning_rate::Float64,
 )
 
-    dE_dw, dE_db = gradient(w, b, data)
+    dE_dw, dE_db = gradient(neuron.w, neuron.b, data)
 
-    w -= learning_rate .* dE_dw
-    b -= learning_rate * dE_db
+    w = neuron.w - learning_rate .* dE_dw
+    b = neuron.b - learning_rate * dE_db
 
-    return w, b
+    return Neuron(w, b)
 end
 
 function one_iteration(
-    w::Vector{Float64},
-    b::Float64,
+    neuron::Neuron,
     data::Vector{Tuple{Vector{Float64},Float64}},
     learning_rate::Float64,
 )
     for d in data
-        w, b = one_step(w, b, d, learning_rate)
+        neuron = one_step(neuron, d, learning_rate)
     end
-    return w, b
+    return neuron
 end
 
 function train(
-    w::Vector{Float64},
-    b::Float64,
+    neuron::Neuron,
     data::Vector{Tuple{Vector{Float64},Float64}},
     learning_rate::Float64,
-    times::Int64,
+    times::Int,
 )
-    while times > 0
-        w, b = one_iteration(w, b, data, learning_rate)
-        times = times - 1
+    for _ = 1:times
+        neuron = one_iteration(neuron, data, learning_rate)
     end
 
-    return w, b
+    return neuron
 end
 
 # Initial values
-w = [0.5, 0.5]
-b = -0.5
-n = 0.0001
+neuron = Neuron([0.5, 0.5], -0.5)
+learn_rate = 0.0001
 
 # Expected results
 data = [([0.0, 0.0], 0.0), ([0.0, 1.0], 0.0), ([1.0, 0.0], 0.0), ([1.0, 1.0], 1.0)]
 
 # Let's do a single iteration
-println("Before first step: w = $w, b = $b")
+println("Before first step: neuron $neuron")
 
-w, b = one_step(w, b, data[1], n)
-println("After first step : w = $w b = $b")
+neuron = one_step(neuron, data[1], learn_rate)
+println("After first step : neuron $neuron")
 
-w, b = one_step(w, b, data[2], n)
-println("After second step: w = $w, b = $b")
+neuron = one_step(neuron, data[2], learn_rate)
+println("After second step: neuron $neuron")
 
-w, b = one_step(w, b, data[3], n)
-println("After third step : w = $w, b = $b")
+neuron = one_step(neuron, data[3], learn_rate)
+println("After third step : neuron $neuron")
 
-w, b = one_step(w, b, data[4], n)
-println("After fourth step: w = $w, b = $b")
+neuron = one_step(neuron, data[4], learn_rate)
+println("After fourth step: neuron $neuron")
 
 println("One iteration done")
 
-w, b = one_iteration(w, b, data, n)
-println(" after step : w = $w, b = $b")
+neuron = one_iteration(neuron, data, learn_rate)
+println(" after step : neuron $neuron")
 
 println("Two iteration done")
